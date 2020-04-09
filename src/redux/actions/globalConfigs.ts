cb84733dcced23c4b3e524aebc05396e8e9bd1a7
Daphne <won.jr@hotmail.com>
2020-04-09 11:06:26 +0800

    Updates

--- src/redux/actions/globalConfigs.ts
+++
@@ -1,8 +0,0 @@
-import Theme from "src/redux/reducers/globalConfigs";
-
-export const SET_THEME = "setTheme";
-
-export const setTheme = (payload: typeof Theme) => ({
-  type: SET_THEME,
-  payload,
-});
